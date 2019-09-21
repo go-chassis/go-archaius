4c819b88866d65f1bad5b08fbd8dc1348d6d0e0c
tian <xiaoliang.tian@gmail.com>
2019-09-21 14:10:33 +0800

    add coveralls

--- archaius_test.go
+++ archaius_test.go
@@ -20,54 +20,61 @@ func (e EListener) Event(event *event.Event) {
 
 var filename2 string
 
-func TestNew(t *testing.T) {
+func TestInit(t *testing.T) {
 	f1Bytes := []byte(`
 age: 14
 name: peter
 `)
 	f2Bytes := []byte(`
-addr: 14
+addr: somewhere
 number: 1
 `)
 	d, _ := os.Getwd()
 	filename1 := filepath.Join(d, "f1.yaml")
 	filename2 = filepath.Join(d, "f2.yaml")
-	os.Remove(filename1)
-	os.Remove(filename2)
 	f1, err := os.Create(filename1)
 	assert.NoError(t, err)
 	defer f1.Close()
+	defer os.Remove(filename1)
 	f2, err := os.Create(filename2)
 	assert.NoError(t, err)
 	defer f2.Close()
+	defer os.Remove(filename2)
 	_, err = io.WriteString(f1, string(f1Bytes))
 	t.Log(string(f1Bytes))
 	assert.NoError(t, err)
 	_, err = io.WriteString(f2, string(f2Bytes))
 	assert.NoError(t, err)
-
+	os.Setenv("age", "15")
 	err = archaius.Init(
 		archaius.WithRequiredFiles([]string{filename1}),
-		archaius.WithOptionalFiles([]string{filename2}))
+		archaius.WithOptionalFiles([]string{filename2}),
+		archaius.WithENVSource(),
+		archaius.WithMemorySource())
 	assert.NoError(t, err)
+	assert.Equal(t, "15", archaius.Get("age"))
+	t.Run("add mem config", func(t *testing.T) {
+		archaius.Set("age", "16")
+		assert.Equal(t, "16", archaius.Get("age"))
+	})
+	t.Run("delete mem config", func(t *testing.T) {
+		archaius.Delete("age")
+		assert.Equal(t, "15", archaius.Get("age"))
+	})
 
 }
 func TestAddFile(t *testing.T) {
 	s := archaius.Get("number")
-	s2 := archaius.Get("age")
-	assert.Equal(t, 14, s2)
 	assert.Equal(t, 1, s)
 
 }
 func TestConfig_Get(t *testing.T) {
-	s := archaius.Get("age")
 	n := archaius.Get("name")
-	assert.Equal(t, 14, s)
 	assert.Equal(t, "peter", n)
 }
 func TestConfig_GetInt(t *testing.T) {
 	s := archaius.Get("age")
-	assert.Equal(t, 14, s)
+	assert.Equal(t, "15", s)
 }
 func TestConfig_RegisterListener(t *testing.T) {
 	eventHandler := EListener{}
@@ -90,8 +97,4 @@ func TestClean(t *testing.T) {
 	s := archaius.Get("age")
 	assert.Equal(t, nil, s)
 
-	err = archaius.Init(
-		archaius.WithOptionalFiles([]string{filename2}))
-	s = archaius.Get("addr")
-	assert.Equal(t, 14, s)
 }
--- scripts/travis/unit_test.sh
+++ scripts/travis/unit_test.sh
@@ -8,6 +8,7 @@ for d in $(go list ./... | grep -v configcenter-source); do
     echo $GOPATH
     cd $GOPATH/src/$d
     if [ $(ls | grep _test.go | wc -l) -gt 0 ]; then
-        go test 
+        go test -v -covermode=count -coverprofile=coverage.out
     fi
 done
+$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci
