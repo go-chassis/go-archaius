62343f62a09621871c292d2446babf11657cbd46
Shawn <xiaoliang.tian@gmail.com>
2019-09-21 17:05:20 +0800

    test all (#82)

--- scripts/travis/unit_test.sh
+++ scripts/travis/unit_test.sh
@@ -2,13 +2,5 @@
 set -e
 
 cd $GOPATH/src/github.com/go-chassis/go-archaius
-#Start unit test
-for d in $(go list ./... | grep -v configcenter-source); do
-    echo $d
-    echo $GOPATH
-    cd $GOPATH/src/$d
-    if [ $(ls | grep _test.go | wc -l) -gt 0 ]; then
-        go test -v -covermode=count -coverprofile=coverage.out
-    fi
-done
+go test ./... -v -covermode=count -coverprofile=coverage.out
 $HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci
