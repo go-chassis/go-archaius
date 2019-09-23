d24c098c5b56cb230c994f2f9c2fe3525fc11062
Shawn <xiaoliang.tian@gmail.com>
2019-09-23 20:20:21 +0800

    add UT (#84)

--- source/remote/remote_test.go
+++ source/remote/remote_test.go
@@ -2,17 +2,72 @@ package remote_test
 
 import (
 	"github.com/go-chassis/go-archaius/event"
-	_ "github.com/go-chassis/go-chassis-config/configcenter"
-
-	"github.com/go-chassis/go-archaius"
 	"github.com/go-chassis/go-archaius/source/remote"
 	"github.com/stretchr/testify/assert"
+	"time"
 
 	"errors"
 	"github.com/go-chassis/go-chassis-config"
 	"testing"
-	"time"
 )
+
+type mockClient struct {
+	opts        config.Options
+	configsInfo map[string]interface{}
+}
+
+// NewClient init the necessary objects needed for seamless communication to Kie Server
+func NewClient(options config.Options) (config.Client, error) {
+	kieClient := &mockClient{
+		opts: options,
+		configsInfo: map[string]interface{}{
+			"some.enable": true,
+		},
+	}
+	return kieClient, nil
+}
+func init() {
+	config.InstallConfigClientPlugin("mock-client", NewClient)
+}
+
+// PullConfigs is used for pull config from servicecomb-kie
+func (c *mockClient) PullConfigs(labels ...map[string]string) (map[string]interface{}, error) {
+
+	return c.configsInfo, nil
+}
+
+// PullConfig get config by key and labels.
+func (c *mockClient) PullConfig(key, contentType string, labels map[string]string) (interface{}, error) {
+	return nil, errors.New("can not find value")
+}
+
+//PushConfigs put config in kie by key and labels.
+func (c *mockClient) PushConfigs(data map[string]interface{}, labels map[string]string) (map[string]interface{}, error) {
+	c.configsInfo = data
+	return nil, nil
+}
+
+//DeleteConfigsByKeys use keyId for delete
+func (c *mockClient) DeleteConfigsByKeys(keys []string, labels map[string]string) (map[string]interface{}, error) {
+	delete(c.configsInfo, keys[0])
+	return nil, nil
+}
+
+//Watch not implemented because kie not support.
+func (c *mockClient) Watch(f func(map[string]interface{}), errHandler func(err error), labels map[string]string) error {
+	// TODO watch change events
+	for {
+		time.Sleep(1 * time.Second)
+		f(c.configsInfo)
+	}
+
+	return errors.New("not implemented")
+}
+
+//Options.
+func (c *mockClient) Options() config.Options {
+	return c.opts
+}
 
 type EventHandler struct {
 	EventName  string
@@ -26,7 +81,7 @@ func (ccenter *EventHandler) OnEvent(event *event.Event) {
 	ccenter.EventValue = event.Value
 }
 
-func TestGetConfigurationsForInvalidCCIP(t *testing.T) {
+func TestNewConfigCenterSource(t *testing.T) {
 	opts := config.Options{
 		Labels: map[string]string{
 			"app":         "default",
@@ -35,115 +90,17 @@ func TestGetConfigurationsForInvalidCCIP(t *testing.T) {
 		TenantName: "default",
 		ServerURI:  "http://",
 	}
-	cc, err := config.NewClient("config_center", opts)
+	cc, err := config.NewClient("mock-client", opts)
 	assert.NoError(t, err)
 	ccs := remote.NewConfigCenterSource(cc, 1,
 		1)
 
-	_, er := ccs.GetConfigurations()
-	if er != nil {
-		assert.Error(t, er)
-	}
-
-	time.Sleep(2 * time.Second)
-	ccs.Cleanup()
-}
-
-func TestGetConfigurationsWithCCIP(t *testing.T) {
-	opts := config.Options{
-		Labels: map[string]string{
-			"app":         "default",
-			"serviceName": "cart",
-		},
-		ServerURI:  "http://",
-		TenantName: "default",
-	}
-	cc, err := config.NewClient("config_center", opts)
+	configs, err := ccs.GetConfigurations()
 	assert.NoError(t, err)
-	ccs := remote.NewConfigCenterSource(cc, 1, 1)
-
-	t.Log("Accessing concenter source configurations")
-	time.Sleep(2 * time.Second)
-	_, er := ccs.GetConfigurations()
-	if er != nil {
-		assert.Error(t, er)
-	}
-	archaius.Init()
-	t.Log("concenter source adding to the archaiuscleanup")
-	e := archaius.AddSource(ccs)
-	if e != nil {
-		assert.Error(t, e)
-	}
-
-	t.Log("verifying configcenter configurations by Configs method")
-	_, err = ccs.GetConfigurationByKey("refreshInterval")
-	if err != nil {
-		assert.Error(t, err)
-	}
-
-	_, err = ccs.GetConfigurationByKey("test")
-	if err != nil {
-		assert.Error(t, err)
-	}
-
-	t.Log("verifying configcenter name")
-	sourceName := ccs.GetSourceName()
-	if sourceName != "ConfigCenterSource" {
-		t.Error("config-center name is mismatched")
-	}
-
-	t.Log("verifying configcenter priority")
-	priority := ccs.GetPriority()
-	if priority != 0 {
-		t.Error("config-center priority is mismatched")
-	}
-
-	t.Log("concenter source cleanup")
-	ccs.Cleanup()
-
-}
-
-func Test_DynamicConfigHandler(t *testing.T) {
-	opts := config.Options{
-		Labels: map[string]string{
-			"app":         "default",
-			"serviceName": "cart",
-		},
-		TenantName: "default",
-		ServerURI:  "http://",
-	}
-	cc, err := config.NewClient("config_center", opts)
-	assert.NoError(t, err)
-	ccs := remote.NewConfigCenterSource(cc, 1, 1)
+	assert.Equal(t, true, configs["some.enable"])
 
 	eh := new(EventHandler)
 
-	ccs.Watch(eh)
-
-	//post the new key, or update the already existing key, or delete the existing key to get the events
-	time.Sleep(4 * time.Second)
-
-	if eh.EventName == "" {
-		err := errors.New("failed to get the event if key is created or updated or deleted")
-		assert.Error(t, err)
-	}
-
-}
-
-func Test_OnReceive(t *testing.T) {
-	opts := config.Options{
-		Labels: map[string]string{
-			"app":         "default",
-			"serviceName": "cart",
-		},
-		TenantName: "default",
-		ServerURI:  "http://",
-	}
-	cc, err := config.NewClient("config_center", opts)
-	assert.NoError(t, err)
-	ccs := remote.NewConfigCenterSource(cc, 1, 1)
-
-	_, er := ccs.GetConfigurations()
-	assert.Error(t, er)
+	_ = ccs.Watch(eh)
 
 }
