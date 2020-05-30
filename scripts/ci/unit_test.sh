132231f136af1737bfc98bd72e7317d43297430f
tian <xiaoliang.tian@gmail.com>
2020-05-30 14:23:05 +0800

    exclude kie test

--- scripts/ci/unit_test.sh
+++ scripts/ci/unit_test.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 set -xe
 
-mkdir -p $GOPATH/src/github.com/apache
-cd $GOPATH/src/github.com/apache
-git clone https://github.com/apache/servicecomb-kie.git
-cd $GOPATH/src/github.com/apache/servicecomb-kie/build
-bash build_docker.sh
-sudo docker-compose -f $GOPATH/src/github.com/apache/servicecomb-kie/deployments/docker/docker-compose.yaml down
-sudo docker-compose -f $GOPATH/src/github.com/apache/servicecomb-kie/deployments/docker/docker-compose.yaml up -d
-
-cd $GOPATH/src/github.com/go-chassis/go-archaius
-go test ./... -v -covermode=count -coverprofile=coverage.out
-$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci
+go test $(go list ./... | grep -v /pkg/kieclient | grep -v /source/remote/kie) -cover
