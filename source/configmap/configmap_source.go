7702d8edaf714785cee2f7fba2b19ae5c919d923
OUYANGKAI <ouyangkai@shopee.com>
2024-03-30 12:53:19 +0800

    fix: confInfo set higher priority filepath

--- source/configmap/configmap_source.go
+++ source/configmap/configmap_source.go
@@ -512,6 +512,7 @@ func (cmSource *configMapSource) compareUpdate(newconf map[string]interface{}, f
 
 				} else if filePathPriority < priority { // lower the vale higher is the priority
 					confInfo.Value = newConfValue
+					confInfo.FilePath = filePath
 					fileConfs[key] = confInfo
 					events = append(events, &event.Event{EventSource: ConfigMapConfigSourceConst,
 						Key: key, EventType: event.Update, Value: newConfValue})
